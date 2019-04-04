f0f7e51fd474e2502ec0c773f7e3e76ff21bcfe0
Prachi Sharma <prachi.sharma2@zensar.com>
2019-04-04 17:30:04 +0530

    Basic node

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var moment = require('moment');
+var bodyParser = require('body-parser');
+// var cookieParser  = require('cookie-parser');
+// var jwt = require('jsonwebtoken');
+// var fs = require("fs");
+//test
+var app = express();
+
+// var appHelpers = {};
+app.use(express.static(__dirname + '/static'));
+
+// app.route('/api/login')
+//     .post(loginRoute);
+
+// const RSA_PRIVATE_KEY = fs.readFileSync('./demos/private.key');
+
+
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/getUnreadNotificationCount/:id', function(req, res, next) {
+	res.send({
+	   "msg": "count",
+	   "success": true,
+		"data":4
+	});
+});
